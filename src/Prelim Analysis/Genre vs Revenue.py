1f08758bf631631b567f88ffb24d3d0e32095ae9
JacobInwald <Jacob.Inwald@Hotmail.com>
2023-03-27 18:16:22 +0100

    Genre vs Revenue Plot

--- src/Prelim Analysis/Genre vs Revenue.py
+++ src/Prelim Analysis/Genre vs Revenue.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from _Data import *
 import seaborn as sns
 
-genreRating = create_average_rating_dict("Genre")
 
 genre_data = pd.read_csv("Data Sets/IMDB-Movie-Data.csv")
 genre_data = genre_data.drop(['Title', 'Description', 'Director', 'Actors', 'Year', 'Runtime (Minutes)', 'Votes'], axis=1)
@@ -18,13 +17,13 @@ for x in range(len(genre_data)):
 genre_data = genre_data_clean
 fig1, ax1 = plt.subplots(figsize=(10, 10))
 ax1.grid(axis="y")
-ax1.set_title("Genre vs Rating")
+ax1.set_title("Genre vs Revenue (Millions)")
 ax1.set_xlabel("Genre")
-ax1.set_ylabel("Rating")
+ax1.set_ylabel("Revenue (Millions)")
 plot = sns.boxplot(data=pd.concat([genre_data, genre_data.assign(type='both')]),
-            x='Genre', y='Rating',
+            x='Genre', y='Revenue (Millions)',
               order=genre_data['Genre'].unique(),
               notch=True,
               color='lightsalmon')
 plt.xticks(rotation=45, ha='right')
-save_figure(plt, "Genre vs Rating")
+save_figure(plt, "Genre vs Revenue (Millions)")
