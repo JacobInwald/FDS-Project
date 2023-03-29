1b96bbaab327f2bf78700a22a99846e76c95b0c6
JacobInwald <Jacob.Inwald@Hotmail.com>
2023-03-29 17:08:31 +0100

    nan

--- src/Prelim Analysis/critic_bot.py
+++ src/Prelim Analysis/critic_bot.py
@@ -1,27 +1,24 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn import preprocessing
-
+import statsmodels.api as sm
+from statsmodels.regression.linear_model import OLS, GLS, WLS
+from sklearn.model_selection import train_test_split
+# reading the csv file
 data = pd.read_csv("Data Sets/normalised_movie_data.csv")
-data = data.drop(['Genre', 'Title', 'Rank'], axis=1)
+data = data.drop(['Genre', 'Title', 'Votes^{1/3}'], axis=1)
 data = data.dropna()
 x = data.drop('Revenue^{1/3}', axis=1)
 y = data['Revenue^{1/3}']
-print(x,y)
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2, random_state=101)
 
-model = LinearRegression()
-model.fit(x_train, y_train)
-predictions = model.predict(x_test)
-print(
-  'mean_squared_error : ', mean_squared_error(y_test, predictions))
-print(
-  'mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3, random_state=101)
 
-print(model.score(x_test, y_test))
-print(cross_val_score(model, x_test, y_test))
+# fit simple linear regression model
+linear_model = OLS(y_train,x_train).fit()
+  
+# display model summary
+print(linear_model.summary())
+fig = plt.figure(figsize=(9,9))
+# creating regression plots
+sm.graphics.plot_partregress_grid(linear_model, fig=fig)
+plt.show()
