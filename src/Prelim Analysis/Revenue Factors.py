06467e5b961df3d6e1e4492dd70899e37d8382b4
Ollie Jones <oathompsonjones@gmail.com>
2023-03-31 11:49:38 +0100

    .

---
+++ src/Prelim Analysis/Revenue Factors.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import re
+import _Data as d
+
+data = pd.read_csv("Data Sets/normalised_movie_data.csv")
+
+fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+fig.suptitle(
+    "The four factors with moderate to strong correlations with a movie's revenue.")
+x = data["Revenue^{1/3}"]
+ys = ["Votes^{1/3}", "ln(Runtime)", "Mean Lead Roles Exp.", "Rank"]
+
+for i, ax in enumerate(axs.flat):
+    y = data[ys[i]]
+    ax.scatter(x, y, s=1)
+    ax.set_ylabel("$Revenue^{\\frac{1}{3}}$")
+    ax.set_xlabel("$" + re.sub(r'(\d)/(\d)',
+                  '\\\\frac{\\1}{\\2}', ys[i]) + "$")
+    a, b = np.polyfit(x, y, 1)
+    r_2 = np.corrcoef(x, y)[0][1]
+    ax.plot(
+        x,
+        a * x + b,
+        label=("$r=$%.2f" % (r_2)),
+        color="red"
+    )
+    ax.legend()
+
+d.save_figure(plt, "Revenue Factors")
--- src/Prelim Analysis/Votes vs Rank.py
+++ src/Prelim Analysis/Votes vs Rank.py
@@ -3,15 +3,15 @@ from _Data import *
 import seaborn as sns
 
 
-data = pd.read_csv("Data Sets/merged_movie_data.csv")
+data = pd.read_csv("Data Sets/normalised_movie_data.csv")
 data = data.dropna()
 
 fig1, ax1 = plt.subplots(figsize=(8, 8))
 ax1.grid(axis="y")
-ax1.set_title("Genre vs Rating^2")
-ax1.set_xlabel("Genre")
-ax1.set_ylabel("Rating^2")
-ax1.scatter(data['Rank'], data['Votes'])
+ax1.set_title("Votes vs Rank")
+ax1.set_xlabel("Rank")
+ax1.set_ylabel("Votes^{1/3}")
+ax1.scatter(data['Rank'], data['Votes^{1/3}'])
 plt.xticks(rotation=45, ha='right')
 
-save_figure(plt, "Votes transform vs Rank")
+save_figure(plt, "Votes vs Rank")
